fa96cca695e2d858e1cfb2c4052ba2f66ac7127c
vanusa <vanusa_marcelino@yahoo.com.br>
2014-08-31 11:05:11 -0300

    Creating AgendaException class comments

--- APB/src/exception/AgendaException.java
+++ APB/src/exception/AgendaException.java
@@ -1,12 +1,18 @@
+/**
+ * AgendaExceptions
+ * AgendaExceptions include constructors to create a exceptions for the erros related to address book.
+ */
 package exception;
 
 @SuppressWarnings("serial")
 public class AgendaException extends Exception {
 
+	/* Constructor creates a AgendaExcpetion without any argument. */
 	public AgendaException() {
 		super();
 	}
 
+	/* Constructor creates a AgendaExcpetion with a message. */
 	public AgendaException(String message) {
 		super(message);
 	}
