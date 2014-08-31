b8ab31b28495e72d782318f7b32553147276662f
vanusa <vanusa_marcelino@yahoo.com.br>
2014-08-31 11:06:09 -0300

    Creating BarbeiroException class comments

--- APB/src/exception/BarbeiroException.java
+++ APB/src/exception/BarbeiroException.java
@@ -1,12 +1,18 @@
+/**
+ * BarbeiroExceptions
+ * BarbeiroExceptions include constructors to create a exceptions for the erros related to barber.
+ */
 package exception;
 
 @SuppressWarnings("serial")
 public class BarbeiroException extends Exception {
 
+	/* Constructor creates a BarbeiroExcpetion without any argument. */
 	public BarbeiroException() {
 		super();
 	}
 
+	/* Constructor creates a BarbeiroExcpetion with a message. */
 	public BarbeiroException(String message) {
 		super(message);
 	}
