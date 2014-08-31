6db5d3caa3b46ed9a172a96f348adcf605f5dcd1
vanusa <vanusa_marcelino@yahoo.com.br>
2014-08-31 11:10:19 -0300

    Creating ServicoException class comments

--- APB/src/exception/ServicoException.java
+++ APB/src/exception/ServicoException.java
@@ -1,12 +1,18 @@
+/**
+ * ServicoExceptions
+ * ServicoExceptions include constructors to create a exceptions for the erros related to service.
+ */
 package exception;
 
 @SuppressWarnings("serial")
 public class ServicoException extends Exception{
 	
+	/* Constructor creates a ServicoExcpetion without any argument. */
 	public ServicoException() {
 		super();
 	}
 
+	/* Constructor creates a ServicoExcpetion with a message. */
 	public ServicoException(String message) {
 		super(message);
 	}
