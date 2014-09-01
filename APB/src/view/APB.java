f589b5a54756f5cb75537a9458649dba4256fe85
LucianoAlmeida <mdiebr@gmail.com>
2014-08-31 22:47:49 -0300

    Creating APB class comments

--- APB/src/view/APB.java
+++ APB/src/view/APB.java
@@ -1,3 +1,9 @@
+/**
+ * APB
+ * This class provides the first window 
+ * to be shown to the user.
+ */
+
 package view;
 
 import java.awt.EventQueue;
@@ -19,9 +25,7 @@ public class APB extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+	/* Launch the application. */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,9 +40,7 @@ public class APB extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	/* Public method to create the frame. */
 	public APB() {
 		setTitle("APB");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,12 +57,17 @@ public class APB extends JFrame {
 		lblBemVindo.setBounds(195, 13, 160, 38);
 		contentPane.add(lblBemVindo);
 
-		JLabel lblAPB = new JLabel("Automa\u00E7\u00E3o de Processos da Barbearia");
+		JLabel lblAPB = new JLabel(
+				"Automa\u00E7\u00E3o de Processos da Barbearia");
 		lblAPB.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAPB.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAPB.setBounds(127, 175, 288, 57);
 		contentPane.add(lblAPB);
 
+		/*
+		 * Add an action performed event. When the Iniciar Button is clicked, it
+		 * takes the user to other window, which is MenuPrincipal.
+		 */
 		JButton btnIniciar = new JButton("Iniciar");
 		btnIniciar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -75,6 +82,10 @@ public class APB extends JFrame {
 		btnIniciar.setBounds(82, 242, 137, 65);
 		contentPane.add(btnIniciar);
 
+		/*
+		 * Add an action performed event. When the Fechar Button is clicked, it
+		 * closes the application.
+		 */
 		JButton btnFechar = new JButton("Fechar");
 		btnFechar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -85,11 +96,13 @@ public class APB extends JFrame {
 				.getResource("/resources/cancel-button-icone-7221-48.png")));
 		btnFechar.setBounds(338, 242, 145, 65);
 		contentPane.add(btnFechar);
-		
+
 		JLabel lblIcone = new JLabel("");
-		lblIcone.setIcon(new ImageIcon(APB.class.getResource("/resources/19fb55_f0fe0bafb3f2bec53376838a10e0825a.jpg_srz_401_146_75_22_0.50_1.20_0.00_jpg_srz.jpg")));
+		lblIcone.setIcon(new ImageIcon(
+				APB.class
+						.getResource("/resources/19fb55_f0fe0bafb3f2bec53376838a10e0825a.jpg_srz_401_146_75_22_0.50_1.20_0.00_jpg_srz.jpg")));
 		lblIcone.setBounds(82, 62, 401, 119);
 		contentPane.add(lblIcone);
-		
+
 	}
 }
