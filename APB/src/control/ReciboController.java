44364d676bb7e219897a16a73857d5a6f8e31d1f
vanusa <vanusa_marcelino@yahoo.com.br>
2014-09-18 23:41:42 -0300

    description ReciboController javaDoc

--- APB/src/control/ReciboController.java
+++ APB/src/control/ReciboController.java
@@ -17,7 +17,11 @@ public class ReciboController {
 	
 	public ReciboController(){}
 	
-	/* Returns an instance of a receipt. */
+	/**
+	 * Provides the singleton implementation
+	 * @return the active ReciboController instance, since it will be just one
+	 * at time
+	 */
 	public static ReciboController getInstance(){
 		if(instance == null){
 			instance = new ReciboController();
@@ -25,10 +29,21 @@ public class ReciboController {
 		return instance;
 	}
 	
-	/* Search a barber's service between two dates. */ 
-	public ResultSet pesquisarServicosDoBarbeiro(String barbeiro, String dataInicial, String dataFinal) throws SQLException{
+	/**
+	 * Search a barber's service between two dates.
+	 * @param  barbeiro one person who works in the barber shops.
+	 * @param dataInicial This represents the date beginning of the service of 
+	 *  the barber.
+	 * @param dataFinal This represents the date ending of the service of 
+	 *  the barber.
+	 * @return Search a barber's service in the database.
+	 * @throws SQLException If has some problem during the database deletion
+	 */
+	public ResultSet pesquisarServicosDoBarbeiro(String barbeiro,
+			String dataInicial, String dataFinal) throws SQLException{
 		
-		return ReciboDAO.getInstance().pesquisarServicosDoBarbeiro(barbeiro, dataInicial, dataFinal);
+		return ReciboDAO.getInstance().pesquisarServicosDoBarbeiro(barbeiro,
+				dataInicial, dataFinal);
 		
 	}
 	
