fa56412a9f83c96c84b561c6eb880cd4c79afa00
vanusa <vanusa_marcelino@yahoo.com.br>
2014-09-22 11:20:42 -0300

    Description TipoServicoController for the javaDoc

--- APB/src/control/TipoServicoController.java
+++ APB/src/control/TipoServicoController.java
@@ -14,10 +14,29 @@ import dao.ServiceTypeDAO;
 import model.TipoServico;
 
 public class TipoServicoController {
+	
+	private TipoServicoController() {}
 
 	private static TipoServicoController instance;
+	
+	/**
+	 * Provides the singleton implementation
+	 * @return the active TipoServicoPrestadoController instance, since it will be just one at
+	 * time.
+	 */
+	public static TipoServicoController getInstance() {
+		if (instance == null)
+			instance = new TipoServicoController();
+		return instance;
+	}
 
-	/* Inserts a new type of service in the database. */
+	/**
+	 * Insert a new type service in the database.
+	 * @param tipoServico instance of a type of object TipoServico.
+	 * @throws SQLException If has some problem during the database insertion
+	 * @return true if no problems.
+	 * @return false if  type of service is null.
+	 */
 	public boolean inserir(TipoServico tipoServico) throws SQLException {
 		if (tipoServico == null) {
 			return false;
@@ -27,7 +46,14 @@ public class TipoServicoController {
 		}
 	}
 
-	/* Alters the name of a type of service in the database. */
+	/**
+	 * Alters a name of a type of service in the database.
+	 * @param nome his represents the new service's name.
+	 * @param tipoServico instance of a type of object TipoServico.
+	 * @return false if tipoServico is null.
+	 * @return true if no problems.
+	 * @throws SQLException If has some problem during the database update
+	 */
 	public boolean alterar(String nome,TipoServico tipoServico) throws SQLException {
 		if (tipoServico == null) {
 			return false;
@@ -38,7 +64,14 @@ public class TipoServicoController {
 		}
 	}
 
-	/* Excludes a type of service from the database. */
+	/**
+	 * Removes a type of service from the database.
+	 * @param tipoServico instance of a type of object TipoServico.
+	 * @throws SQLException If has some problem during the database insertion
+	 * @return true if no problems.
+	 * @return false if  type of service is null.
+	 */
+	
 	public boolean excluir(TipoServico tipoServico) throws SQLException {
 
 		if (tipoServico == null) {
@@ -48,24 +81,24 @@ public class TipoServicoController {
 			return true;
 		}
 	}
-
-	/* Constructor. */
-	private TipoServicoController() {
-	}
-
-	/* Gets and instance from TipoServicoController. */
-	public static TipoServicoController getInstance() {
-		if (instance == null)
-			instance = new TipoServicoController();
-		return instance;
-	}
 	
-	/* Shows all types of service registered in the database. */
+	/**
+	 * Displays all types of service registered in the database.
+	 * @param tipoServico instance of a type of object TipoServico.
+	 * @return Show all types of service registered in the database.
+	 * @throws SQLException If has some problem during the database deletion
+	 */
 	public ResultSet mostrarTipoServicoCadastrados(TipoServico servico) throws SQLException {
 		return ServiceTypeDAO.getInstance().mostrarTipoServicoCadastrados(servico);
 	}
 	
 	/* Search a service by name. */
+	/**
+	 * Displays types of service by name in the database.
+	 * @param servico instance of an type of object TipoServico.
+	 * @return Show a service by name in the database.
+	 * @throws SQLException If has some problem during the database deletion
+	 */
 	public ResultSet pesquisarPorNome(TipoServico servico) throws SQLException {
 		return ServiceTypeDAO.getInstance().pesquisarPorNome(servico);
 	}
