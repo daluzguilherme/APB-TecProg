b06df4c52378d41980a68975d080b27bce4d0253
Guilherme da Luz <guilherme.daluz@outlook.com>
2014-10-03 21:35:19 -0300

    Javadoc comments on ServiceTypeDAO

--- APB/src/dao/ServiceTypeDAO.java
+++ APB/src/dao/ServiceTypeDAO.java
@@ -16,19 +16,35 @@ public class ServiceTypeDAO {
 	/* Instance to the singleton. */
 	private static ServiceTypeDAO instance;
 
+	/**
+	 * Class Constructor
+	 */
 	private ServiceTypeDAO() {
 		
 		/* Blank constructor. */
 	}
 
-	/* Singleton implementation. */
+	/**
+	 * Singleton implementation
+	 * 
+	 * @return instance the active AddressBookDAO instance, since it will be just one
+	 * 					at a time
+	 */
 	public static ServiceTypeDAO getInstance() {
 		if (instance == null)
 			instance = new ServiceTypeDAO();
 		return instance;
 	}
 
-	/* Include new service type in the database. */
+	/**
+	 * Include new service type in the database.
+	 * 
+	 * @param tipoServico a new type of service to be created.
+	 * @return true if it was included successfully
+	 * @return false if type of service is null;
+	 * @throws SQLException If there was some problem during the database
+	 * 						deletion
+	 */
 	public boolean incluir(ServiceType tipoServico) throws SQLException {
 		if (tipoServico == null)
 			return false;
@@ -41,7 +57,16 @@ public class ServiceTypeDAO {
 		return true;
 	}
 
-	/* This updates a service type on the database. */
+	/**
+	 * This updates a service type on the database
+	 * 
+	 * @param nome a name to be altered.
+	 * @param tipoServico_alterado an old type of service altered
+	 * @param tipoServico a service type to be altered.
+	 * @return true if it was altered successfully. 
+	 * @throws SQLException If there was some problem during the database
+	 * 						deletion.
+	 */
 	public boolean alterar(String nome,ServiceType tipoServico_alterado, ServiceType tipoServico) throws SQLException {
 		if (tipoServico_alterado == null || tipoServico == null) 
 			return false;
@@ -54,7 +79,15 @@ public class ServiceTypeDAO {
 		return true;
 	}
 
-	/* This removes a service type from the database. */
+	/**
+	 * This removes a service type from the database
+	 * 
+	 * @param tipoServico a type of service to be deleted
+	 * @return true if everything went OK.
+	 * @return false if type of service is null.
+	 * @throws SQLException If there was some problem during the database
+	 * 						deletion
+	 */
 	public boolean excluir(ServiceType tipoServico) throws SQLException {
 		if (tipoServico == null)
 			return false;
@@ -64,7 +97,13 @@ public class ServiceTypeDAO {
 		return true;
 	}
 
-	/* This updates a query. */
+	/**
+	 * This updates a query
+	 * 
+	 * @param message gives a message to update the query.
+	 * @throws SQLException If there was some problem during the database
+	 * 						deletion
+	 */
 	public void updateQuery(String message) throws SQLException {
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		PreparedStatement preparedStatement = connection.prepareStatement(message);
@@ -73,7 +112,14 @@ public class ServiceTypeDAO {
 		connection.close();
 	}
 	
-	/* Shows registered services type */
+	/**
+	 * Shows registered services type
+	 * 
+	 * @param servico NOT USED
+	 * @return rs a ResultSet with the services registered
+	 * @throws SQLException If there was some problem during the database
+	 * 						deletion.
+	 */
 	public ResultSet mostrarTipoServicoCadastrados(ServiceType servico) throws SQLException {
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		ResultSet rs = connection.createStatement().executeQuery(
@@ -82,7 +128,14 @@ public class ServiceTypeDAO {
 		return rs;
 	}
 	
-	/* Search by name */
+	/**
+	 * Search a service by name
+	 * 
+	 * @param servico a service to be searched
+	 * @return rs a ResultSet with the services found
+	 * @throws SQLException If there was some problem during the database
+	 * 						deletion.
+	 */
 	public ResultSet pesquisarPorNome(ServiceType servico) throws SQLException {
 		Connection connection = FactoryConnection.getInstance().getConnection();
 		java.sql.PreparedStatement pst = connection.prepareStatement("SELECT * FROM tiposervico WHERE "
