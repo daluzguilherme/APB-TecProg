c34fdccb0bed754523468d46b10844b2703faa1d
LucianoAlmeida <mdiebr@gmail.com>
2014-11-30 22:33:25 -0200

    Translating NewProvidedService variables and methods

--- APB/src/view/NewProvidedService.java
+++ APB/src/view/NewProvidedService.java
@@ -41,15 +41,15 @@ import java.awt.event.ItemEvent;
 public class NewProvidedService extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textValor;
+	private JTextField textFieldServicePriceNumber;
 
 	/* Launch the application. */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					NewProvidedService frame = new NewProvidedService();
-					frame.setVisible(true);
+					NewProvidedService newNewProvidedServiceWindow = new NewProvidedService();
+					newNewProvidedServiceWindow.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -68,29 +68,29 @@ public class NewProvidedService extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblServico = new JLabel("Servi\u00E7o:");
-		lblServico.setBounds(27, 25, 46, 14);
-		contentPane.add(lblServico);
+		JLabel lblStringService = new JLabel("Servi\u00E7o:");
+		lblStringService.setBounds(27, 25, 46, 14);
+		contentPane.add(lblStringService);
 
-		JLabel lblRealizadoPor = new JLabel("Realizado por:");
-		lblRealizadoPor.setBounds(27, 56, 92, 14);
-		contentPane.add(lblRealizadoPor);
+		JLabel lblStringProvidedBy = new JLabel("Realizado por:");
+		lblStringProvidedBy.setBounds(27, 56, 92, 14);
+		contentPane.add(lblStringProvidedBy);
 
-		JLabel lblPreco = new JLabel("Pre\u00E7o (R$):");
-		lblPreco.setBounds(27, 87, 71, 14);
-		contentPane.add(lblPreco);
+		JLabel lblStringPrice = new JLabel("Pre\u00E7o (R$):");
+		lblStringPrice.setBounds(27, 87, 71, 14);
+		contentPane.add(lblStringPrice);
 
-		textValor = new JTextField();
-		textValor.setColumns(10);
-		textValor.setBounds(129, 84, 114, 20);
-		contentPane.add(textValor);
+		textFieldServicePriceNumber = new JTextField();
+		textFieldServicePriceNumber.setColumns(10);
+		textFieldServicePriceNumber.setBounds(129, 84, 114, 20);
+		contentPane.add(textFieldServicePriceNumber);
 
 		/* Create a combo box to put all the barber's name. */
-		final JComboBox comboBoxBarbeiro = new JComboBox();
-		comboBoxBarbeiro.setModel(new DefaultComboBoxModel(
+		final JComboBox comboBoxBarber = new JComboBox();
+		comboBoxBarber.setModel(new DefaultComboBoxModel(
 				new String[] { "Selecione um barbeiro" }));
-		comboBoxBarbeiro.setBounds(129, 53, 289, 20);
-		contentPane.add(comboBoxBarbeiro);
+		comboBoxBarber.setBounds(129, 53, 289, 20);
+		contentPane.add(comboBoxBarber);
 
 		/*
 		 * Some refactoring is needed here. Accessing the database direct from a
@@ -100,25 +100,25 @@ public class NewProvidedService extends JFrame {
 		/*
 		 * Getting the price value from the database to fill a text field.
 		 */
-		final JComboBox comboBoxServico = new JComboBox();
-		comboBoxServico.addItemListener(new ItemListener() {
+		final JComboBox comboBoxTypeOfService = new JComboBox();
+		comboBoxTypeOfService.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent arg0) {
 				Connection connection;
-				if (comboBoxServico.getSelectedIndex() != 0)
+				if (comboBoxTypeOfService.getSelectedIndex() != 0)
 					try {
-						String[] nome = comboBoxServico.getSelectedItem()
+						String[] typeOfServiceNameSplit = comboBoxTypeOfService.getSelectedItem()
 								.toString().split(" - ");
 						connection = FactoryConnection.getInstance()
 								.getConnection();
 						java.sql.PreparedStatement pst1 = connection
 								.prepareStatement("SELECT preco FROM tipoServico WHERE nome = \""
-										+ nome[1] + "\";");
+										+ typeOfServiceNameSplit[1] + "\";");
 						ResultSet rs1 = pst1.executeQuery();
 						rs1.next();
 
-						textValor.setText(rs1.getString("preco"));
+						textFieldServicePriceNumber.setText(rs1.getString("preco"));
 					} catch (SQLException e) {
-						mostrarMensagemDeErro(e.getMessage());
+						showErrorMessage(e.getMessage());
 					}
 			}
 
@@ -129,13 +129,13 @@ public class NewProvidedService extends JFrame {
 		 * chair. Populating the combo box Servico with the type of the service
 		 * done by the barber to the client.
 		 */
-		comboBoxServico.setModel(new DefaultComboBoxModel(
+		comboBoxTypeOfService.setModel(new DefaultComboBoxModel(
 				new String[] { "Selecione um tipo de servi\u00E7o" }));
-		comboBoxServico.setMaximumRowCount(4);
-		comboBoxServico.setBounds(129, 22, 289, 20);
-		contentPane.add(comboBoxServico);
+		comboBoxTypeOfService.setMaximumRowCount(4);
+		comboBoxTypeOfService.setBounds(129, 22, 289, 20);
+		contentPane.add(comboBoxTypeOfService);
 		try {
-			int cont = 0;
+			int typeOfServiceCounter = 0;
 			Connection connection = FactoryConnection.getInstance()
 					.getConnection();
 			java.sql.PreparedStatement pst = connection
@@ -146,18 +146,18 @@ public class NewProvidedService extends JFrame {
 			ResultSet rs2 = pst2.executeQuery();
 
 			while (rs.next()) {
-				String nome = rs.getString("nome");
-				String cadeira = rs.getString("cadeira");
-				comboBoxBarbeiro.addItem(cadeira + " - " + nome);
+				String barberName = rs.getString("nome");
+				String barberChair = rs.getString("cadeira");
+				comboBoxBarber.addItem(barberChair + " - " + barberName);
 			}
 			while (rs2.next()) {
-				cont++;
-				String nome = rs2.getString("nome");
-				comboBoxServico.addItem(cont + " - " + nome);
+				typeOfServiceCounter++;
+				String typeOfServiceName = rs2.getString("nome");
+				comboBoxTypeOfService.addItem(typeOfServiceCounter + " - " + typeOfServiceName);
 			}
 
 		} catch (SQLException e) {
-			mostrarMensagemDeErro(e.getMessage());
+			showErrorMessage(e.getMessage());
 		}
 
 		/*
@@ -165,104 +165,104 @@ public class NewProvidedService extends JFrame {
 		 * takes the strings in all fields and creates a new entry in the
 		 * database.
 		 */
-		JButton botaoSalvar = new JButton("Salvar");
-		botaoSalvar.addMouseListener(new MouseAdapter() {
+		JButton buttonRegisterNewProvidedService = new JButton("Salvar");
+		buttonRegisterNewProvidedService.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				try {
-					if (comboBoxServico.getSelectedIndex() == 0)
+					if (comboBoxTypeOfService.getSelectedIndex() == 0)
 						JOptionPane.showMessageDialog(null,
 								"Você deve selecionar um tipo de serviço.");
-					else if (comboBoxBarbeiro.getSelectedIndex() == 0)
+					else if (comboBoxBarber.getSelectedIndex() == 0)
 						JOptionPane.showMessageDialog(null,
 								"Você deve selecionar um barbeiro.");
 					else {
-						String data;
-						Date d = new Date();
-						SimpleDateFormat sdf = new SimpleDateFormat(
+						String formatedIsoDate;
+						Date systemDateInformation = new Date();
+						SimpleDateFormat isoDateFormat = new SimpleDateFormat(
 								"yyyy-MM-dd");
-						data = sdf.format(d);
+						formatedIsoDate = isoDateFormat.format(systemDateInformation);
 
-						String[] nome = comboBoxServico.getSelectedItem()
+						String[] selectedTypeOfServiceInformationSplit = comboBoxTypeOfService.getSelectedItem()
 								.toString().split(" - ");
-						String[] barbeiro = comboBoxBarbeiro.getSelectedItem()
+						String[] selectedBarberInformationSplit = comboBoxBarber.getSelectedItem()
 								.toString().split(" - ");
 
-						ProvidedService servico_prestado = new ProvidedService();
+						ProvidedService providedService = new ProvidedService();
 
-						servico_prestado.setNomeBarbeiro(barbeiro[1]);
-						servico_prestado.setNomeServico(nome[1]);
-						servico_prestado.setPreco(textValor.getText());
-						servico_prestado.setData(data);
+						providedService.setNomeBarbeiro(selectedBarberInformationSplit[1]);
+						providedService.setNomeServico(selectedTypeOfServiceInformationSplit[1]);
+						providedService.setPreco(textFieldServicePriceNumber.getText());
+						providedService.setData(formatedIsoDate);
 
-						ProvidedServiceController servicoController = ProvidedServiceController
+						ProvidedServiceController providedServiceController = ProvidedServiceController
 								.getInstance();
-						servicoController.inserir(servico_prestado);
+						providedServiceController.inserir(providedService);
 
 						JOptionPane.showMessageDialog(null,
 								"Serviço criado com sucesso");
 
-						comboBoxBarbeiro.setSelectedIndex(0);
-						comboBoxServico.setSelectedIndex(0);
+						comboBoxBarber.setSelectedIndex(0);
+						comboBoxTypeOfService.setSelectedIndex(0);
 
-						textValor.setText("");
+						textFieldServicePriceNumber.setText("");
 					}
 				} catch (ServiceException e) {
-					mostrarMensagemDeErro(e.getMessage());
+					showErrorMessage(e.getMessage());
 				} catch (SQLException e) {
-					mostrarMensagemDeErro(e.getMessage());
+					showErrorMessage(e.getMessage());
 				} catch (ParseException e) {
-					mostrarMensagemDeErro(e.getMessage());
+					showErrorMessage(e.getMessage());
 				}
 
 			}
 		});
-		botaoSalvar.setBounds(27, 129, 89, 23);
-		contentPane.add(botaoSalvar);
+		buttonRegisterNewProvidedService.setBounds(27, 129, 89, 23);
+		contentPane.add(buttonRegisterNewProvidedService);
 
 		/*
 		 * Add a mouse clicked event. When the LimparCampos Button is clicked,
 		 * it clears all the text fields.
 		 */
-		JButton botaoLimparCampos = new JButton("Limpar Campos");
-		botaoLimparCampos.addMouseListener(new MouseAdapter() {
+		JButton buttonClearTextFields = new JButton("Limpar Campos");
+		buttonClearTextFields.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				textValor.setText("");
-				comboBoxBarbeiro.setSelectedIndex(0);
-				comboBoxServico.setSelectedIndex(0);
+				textFieldServicePriceNumber.setText("");
+				comboBoxBarber.setSelectedIndex(0);
+				comboBoxTypeOfService.setSelectedIndex(0);
 			}
 		});
-		botaoLimparCampos.setBounds(152, 129, 148, 23);
-		contentPane.add(botaoLimparCampos);
+		buttonClearTextFields.setBounds(152, 129, 148, 23);
+		contentPane.add(buttonClearTextFields);
 
 		/*
 		 * Add a mouse clicked event. When the Voltar Button is clicked, it
 		 * returns the the previous window, which is RegisterBarber.
 		 */
-		JButton botaoVoltar = new JButton("Voltar");
-		botaoVoltar.addMouseListener(new MouseAdapter() {
+		JButton buttonReturn = new JButton("Voltar");
+		buttonReturn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				dispose();
-				RegisterProvidedService frame = new RegisterProvidedService();
-				frame.setVisible(true);
-				frame.setLocationRelativeTo(null);
+				RegisterProvidedService newRegisterProvidedServiceWindow = new RegisterProvidedService();
+				newRegisterProvidedServiceWindow.setVisible(true);
+				newRegisterProvidedServiceWindow.setLocationRelativeTo(null);
 			}
 		});
-		botaoVoltar.setBounds(329, 129, 89, 23);
-		contentPane.add(botaoVoltar);
+		buttonReturn.setBounds(329, 129, 89, 23);
+		contentPane.add(buttonReturn);
 	}
 
 	/**
 	 * This method shows an error message.
 	 * 
-	 * @param informacao
+	 * @param errorMessage
 	 *            A String type variable that contains the error message to be
 	 *            shown to the user.
 	 */
-	private void mostrarMensagemDeErro(String informacao) {
-		JOptionPane.showMessageDialog(null, informacao, "Atenção",
+	private void showErrorMessage(String errorMessage) {
+		JOptionPane.showMessageDialog(null, errorMessage, "Atenção",
 				JOptionPane.INFORMATION_MESSAGE);
 	}
 }
